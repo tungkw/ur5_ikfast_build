27bf0daa2ffe6e755c464c978ec33868416244db
tungkw <914989053@qq.com>
2021-03-09 16:22:11 +0800

    update

---
+++ xacro_to_collada.sh
@@ -0,0 +1,6 @@
+rm ./urdf/ur5.urdf
+rm ./urdf/ur5.dae
+
+rosrun xacro xacro --inorder -o ./urdf/ur5.urdf ./urdf/ur5.urdf.xacro
+rosrun collada_urdf urdf_to_collada ./urdf/ur5.urdf ./urdf/ur5.dae
+#rosrun moveit_kinematics round_collada_numbers.py ./urdf/ur5.dae ./urdf/ur5.dae 5
