de5b34d8f1d0558d1cff4656191ab3bfd2a79138
tungkw <914989053@qq.com>
2021-03-10 17:13:46 +0800

    real ikfast success

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ rm ./bin/ikfast
 
 # build file
 openrave-robot.py ./urdf/ur5.dae --info links
-python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=./urdf/ur5.dae --iktype=transform6d --baselink=0 --eelink=10 --savefile=./src/ur5_ikfast.cpp
+python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=./urdf/ur5.dae --iktype=transform6d --baselink=0 --eelink=9 --savefile=./src/ur5_ikfast.cpp
 g++ ./src/ur5_ikfast.cpp -o ./bin/ikfast -llapack -std=c++11 -I `rospack find ur5_ikfast_plugin`/include/
 rosrun moveit_kinematics create_ikfast_moveit_plugin.py ur5 manipulator ur5_ikfast_plugin base_link ee_link ./src/ur5_ikfast.cpp
