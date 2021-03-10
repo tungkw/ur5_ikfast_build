4f03c5268be41a5b560901f13a8037e254a41332
tungkw <914989053@qq.com>
2021-03-11 01:03:29 +0800

    calibration xacro for openrave ikfast

--- xacro_to_collada.sh
+++ xacro_to_collada.sh
@@ -1,7 +1,7 @@
 rm ./urdf/ur5.urdf
 rm ./urdf/ur5.dae
 
-#rosrun xacro xacro --inorder -o ./urdf/ur5.urdf ./urdf/ur5.urdf.xacro
-rosrun xacro xacro --inorder -o ./urdf/ur5.urdf `rospack find ur_description`/urdf/ur5_robot.urdf.xacro
+rosrun xacro xacro --inorder -o ./urdf/ur5.urdf ./urdf/ur5.urdf.xacro
+#rosrun xacro xacro --inorder -o ./urdf/ur5.urdf `rospack find ur_description`/urdf/ur5_robot.urdf.xacro
 rosrun collada_urdf urdf_to_collada ./urdf/ur5.urdf ./urdf/ur5.dae
 #rosrun moveit_kinematics round_collada_numbers.py ./urdf/ur5.dae ./urdf/ur5.dae 5
